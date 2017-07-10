205715b8edef24984cef99cc4efe81d2e9fc3cd6
Ivan Sambrana <sambranaivan@gmail.com>
2017-07-10 15:17:47 -0300

    reorganizacion

--- app/src/gpioInterrupt.c
+++ app/src/gpioInterrupt.c
@@ -79,132 +79,135 @@ void STATE_MACHINE()
 {
     
     
-switch (STATE)
-	{
-		case 0:
-			if(check_state(1,0,0)){
-				STATE = 1;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 1:
-			if(check_state (0,0,0)){
-				STATE = 2;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 2:
-			if(check_state(1,1,0)){
-				STATE = 3;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 3:
-			if(check_state(0,1,0)){
-				STATE = 4;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 4:
-			if(check_state(1,0,0)){
-				STATE = 5;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 5:
-			if(check_state(0,0,0)){
-				STATE = 6;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 6:
-			if(check_state(1,0,0)){
-				STATE = 7;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 7:
-			if(check_state(0,0,0)){
-				STATE = 8;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 8:
-			if(check_state(1,0,0)){
-				STATE = 9;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 9:
-			if(check_state(0,0,0)){
-				STATE = 10;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 10:
-			if(check_state(1,0,1)){
-				STATE = 11;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-		case 11:
-			if(check_state(0,0,1)){
-				STATE = 0;
-			}
-      else
-        {
-          STATE = 13;
-        }
-		break;
-    case 12:
-        if(check_state(0,0,1))
-        {
-            gpioWrite(LEDB,OFF);
-            gpioWrite(LEDG,!gpioRead(PIN_1));
-            
-            LEDCONTROL = LEDG;
-            STATE = 0;
-        }
-    break;
-       case 13:
-        gpioWrite(LEDG,OFF);
-            gpioWrite(LEDR,!gpioRead(PIN_1));
-            LEDCONTROL = LEDR;
-    break;
-	}
+  switch (STATE)
+  	{
+  		case 0:
+  			if(check_state(1,0,0)){
+  				STATE = 1;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 1:
+  			if(check_state (0,0,0)){
+  				STATE = 2;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 2:
+  			if(check_state(1,1,0)){
+  				STATE = 3;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 3:
+  			if(check_state(0,1,0)){
+  				STATE = 4;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 4:
+  			if(check_state(1,0,0)){
+  				STATE = 5;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 5:
+  			if(check_state(0,0,0)){
+  				STATE = 6;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 6:
+  			if(check_state(1,0,0)){
+  				STATE = 7;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 7:
+  			if(check_state(0,0,0)){
+  				STATE = 8;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 8:
+  			if(check_state(1,0,0)){
+  				STATE = 9;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 9:
+  			if(check_state(0,0,0)){
+  				STATE = 10;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 10:
+  			if(check_state(1,0,1)){
+  				STATE = 11;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+  		case 11:
+  			if(check_state(0,0,1)){
+  				STATE = 0;
+  			}
+        else
+          {
+            STATE = 13;
+          }
+  		break;
+      case 12:
+          if(check_state(0,0,1))
+          {
+
+              gpioWrite(LEDB,OFF);
+              gpioWrite(LEDG,!gpioRead(PIN_1));
+              LEDCONTROL = LEDG;
+            ///SEÑAL SINCRONIZADA IR AL ESTADO INICIAL
+              
+              STATE = 0;
+          }
+      break;
+         case 13:
+              gpioWrite(LEDG,OFF);
+              gpioWrite(LEDR,!gpioRead(PIN_1));
+              //ESTADO DE ERROR 
+              LEDCONTROL = LEDR;
+      break;
+  	}
 
 }
 
