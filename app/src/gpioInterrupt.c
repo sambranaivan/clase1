200472b9f7b2f482b3ae85839ced7967970173a0
Ivan Sambrana <sambranaivan@gmail.com>
2017-07-18 11:35:58 -0300

    jenkisn integration

--- app/src/gpioInterrupt.c
+++ app/src/gpioInterrupt.c
@@ -76,12 +76,16 @@ char* itoa(int value, char* result, int base) {
 void obtenerDatos()
 {
     uint8_t dataToReadBuffer;
+    uint8_t Address;
     char xresult;
     char buff[10];
+    int buffsize = 4;
    dataToReadBuffer = 0x00;
-   i2cRead( I2C0, 0x01,
+    Address = 0x03;
+    //Address = 0x00;
+   i2cRead( I2C0, Address,
             &dataToReadBuffer, 1, TRUE,
-            &xresult, 4, TRUE );
+            &xresult, buffsize, TRUE );
     
     //itoa(xresult,buff,10);
     uartWriteString(UART_USB,&xresult);
